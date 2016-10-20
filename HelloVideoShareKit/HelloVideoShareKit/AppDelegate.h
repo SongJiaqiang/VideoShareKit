f8af6c0968e044da9e5e8c7006608dd9d3015ab1
Jarvis <sjq2150@gmail.com>
2016-10-20 09:56:07 +0800

    Add VideoShareKit demo

---
+++ HelloVideoShareKit/HelloVideoShareKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloVideoShareKit
+//
+//  Created by Jarvis on 20/10/2016.
+//  Copyright © 2016 Jarvis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloVideoShareKit/HelloVideoShareKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloVideoShareKit
+//
+//  Created by Jarvis on 20/10/2016.
+//  Copyright © 2016 Jarvis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
